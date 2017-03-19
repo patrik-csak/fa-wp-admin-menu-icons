5300135df369acc0a176e4bfef4f063a219d8976
Patrik Csak <patrik@csak.email>
2017-03-19 11:17:45 -0700

    Enable Font Awesome icons for admin menu pages

--- font-awesome-admin-menu-icons.php
+++ font-awesome-admin-menu-icons.php
@@ -16,8 +16,8 @@
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with Font Awesome Custom
- * Post Type Menu Icons. If not, see https://www.gnu.org/licenses/gpl.html.
+ * You should have received a copy of the GNU General Public License along with Font Awesome Admin
+ * Menu Icons. If not, see https://www.gnu.org/licenses/gpl.html.
  */
 
 namespace FAAMI;
@@ -27,30 +27,66 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 /**
+ * Get the icon name from a string formatted as 'fa-<icon>'
+ *
+ * @param string $fa_string String with starts with 'fa-'
+ *
+ * @return string
+ */
+function get_icon_name( $fa_string ) {
+	preg_match( '/fa-(.*)/', $fa_string, $matches );
+
+	return $matches[1];
+}
+
+/**
+ * Get the path to the icon
+ *
+ * @param string $icon_name Name of a Font Awesome icon (not prepended with 'fa-'
+ *
+ * @return string
+ */
+function get_icon_path( $icon_name ) {
+	return plugin_dir_path( __FILE__ ) . "icons/$icon_name.svg";
+}
+
+/**
+ * Base64 encode the SVG and prepend with necessary code to make it usable as a data URI
+ *
+ * Also add fill color to the SVG
+ *
+ * @param string $path Path to the icon file
+ *
+ * @return string
+ */
+function get_icon_svg_data_uri( $path ) {
+	$svg_contents = file_get_contents( $path );
+	$svg          = new \SimpleXMLElement( $svg_contents );
+	/**
+	 * Add necessary fill color to make SVG display properly
+	 *
+	 * Using Yoast's fill color:
+	 * @see https://github.com/Yoast/wordpress-seo/blob/4.4/inc/class-wpseo-utils.php#L916
+	 */
+	$svg->addAttribute( 'style', 'fill:#82878c' );
+
+	return 'data:image/svg+xml;base64,' . base64_encode( $svg->asXML() );
+}
+
+/**
  * Add Font Awesome icon to {@see \register_post_type()} args
  *
  * @param array $args The arguments passed to {@see \register_post_type()}
  *
  * @return array
  */
-function add_fa_icon( $args ) {
+function post_type_font_awesome_icon( $args ) {
 	// Does the `menu_icon` arg start with 'fa-'?
 	if ( strpos( $args['menu_icon'], 'fa-' ) === 0 ) {
-		// Get the icon name
-		preg_match( '/fa-(.*)/', $args['menu_icon'], $matches );
-		$icon      = $matches[1];
-		$icon_path = plugin_dir_path( __FILE__ ) . "icons/$icon.svg";
+		$icon      = get_icon_name( $args['menu_icon'] );
+		$icon_path = get_icon_path( $icon );
 		if ( file_exists( $icon_path ) ) {
-			$svg_contents = file_get_contents( $icon_path );
-			$svg          = new \SimpleXMLElement( $svg_contents );
-			/**
-			 * Add necessary fill color to make SVG display properly
-			 *
-			 * Using Yoast's fill color:
-			 * @see https://github.com/Yoast/wordpress-seo/blob/4.4/inc/class-wpseo-utils.php#L916
-			 */
-			$svg->addAttribute( 'style', 'fill:#82878c' );
-			$args['menu_icon'] = 'data:image/svg+xml;base64,' . base64_encode( $svg->asXML() );
+			$args['menu_icon'] = get_icon_svg_data_uri( $icon_path );
 		} else {
 			/**
 			 * Couldn't find the icon, so there's nothing we can do. If we leave `menu_icon` as
@@ -66,4 +102,27 @@ function add_fa_icon( $args ) {
 	return $args;
 }
 
-add_filter( 'register_post_type_args', __NAMESPACE__ . '\\add_fa_icon' );
+add_filter( 'register_post_type_args', __NAMESPACE__ . '\\post_type_font_awesome_icon' );
+
+function menu_page_font_awesome_icon( $url ) {
+	if ( strpos( $url, 'fa-' ) === 0 ) {
+		$icon      = get_icon_name( $url );
+		$icon_path = get_icon_path( $icon );
+		if ( file_exists( $icon_path ) ) {
+			return get_icon_svg_data_uri( $icon_path );
+		} else {
+			/**
+			 * Couldn't find the icon, so there's nothing we can do. Resetting the url to
+			 * `dashicons-admin-generic`, because that's what WP does if you don't pass an `icon_url`
+			 * parameter to {@see add_menu_page()}
+			 *
+			 * @see https://github.com/WordPress/WordPress/blob/4.7.3/wp-admin/includes/plugin.php#L1087
+			 */
+			return 'dashicons-admin-generic';
+		}
+	} else {
+		return $url;
+	}
+}
+
+add_filter( 'set_url_scheme', __NAMESPACE__ . '\\menu_page_font_awesome_icon' );
