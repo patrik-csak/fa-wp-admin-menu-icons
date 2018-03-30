c7b23eb498bd26dff7ec4846484ac4b8213342c7
Patrik Csak <patrik@csak.email>
2018-03-29 20:40:57 -0700

    Bump Font Awesome version
    From 5.0.8 to 5.0.9

--- src/Fawpami.php
+++ src/Fawpami.php
@@ -10,7 +10,7 @@ require_once 'Version.php';
 class Fawpami
 {
     /** @var string */
-    const FA_VERSION = '5.0.8';
+    const FA_VERSION = '5.0.9';
 
     /** @var AdminNotices */
     public $adminNotices;
