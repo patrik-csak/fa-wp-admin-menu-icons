9e9d220dd273488cb048156b234ac2a993b9ae1a
Patrik Csak <patrik@csak.email>
2018-04-21 11:49:23 -0700

    Font Awesome v5.0.9 → v5.0.10

--- src/Fawpami.php
+++ src/Fawpami.php
@@ -10,7 +10,7 @@ require_once 'Version.php';
 class Fawpami
 {
     /** @var string */
-    const FA_VERSION = '5.0.9';
+    const FA_VERSION = '5.0.10';
 
     /** @var AdminNotices */
     public $adminNotices;
