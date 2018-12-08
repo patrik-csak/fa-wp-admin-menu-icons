c3f2f058a092b2720661db2e9d931a16eedc5635
Patrik Csak <patrik@csak.email>
2018-12-07 20:44:09 -0500

    Wrap at 80 lines

--- fa-wp-admin-menu-icons.php
+++ fa-wp-admin-menu-icons.php
@@ -8,16 +8,18 @@
  * Author URI: https://github.com/ptrkcsk
  * License: GPL
  *
- * FA WP Admin Menu Icons is free software: you can redistribute it and/or modify it under the terms
- * of the GNU General Public License as published by the Free Software Foundation, either version 2
- * of the License, or any later version.
+ * FA WP Admin Menu Icons is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or any later
+ * version.
  *
- * FA WP Admin Menu Icons is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- * PURPOSE. See the GNU General Public License for more details.
+ * FA WP Admin Menu Icons is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License along with FA WP Admin Menu
- * Icons. If not, see https://www.gnu.org/licenses/gpl.html.
+ * You should have received a copy of the GNU General Public License along with
+ * FA WP Admin Menu Icons. If not, see https://www.gnu.org/licenses/gpl.html.
  */
 
 namespace Fawpami;
