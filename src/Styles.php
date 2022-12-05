1a6ec7d2f07eaa4cea3fb6adc2df949f792d635d
Patrik Csak <p@trikcsak.com>
2022-12-04 19:54:02 -0800

    add comment

--- src/Styles.php
+++ src/Styles.php
@@ -8,7 +8,12 @@ class Styles
     {
         wp_add_inline_style(
             'admin-menu',
-            '#adminmenu div.wp-menu-image--fawpami.svg { background-size: 20px 20px }',
+            <<<'CSS'
+/* FA WP Admin Menu Icons icon styles */
+#adminmenu div.wp-menu-image--fawpami.svg {
+    background-size: 20px 20px;
+}
+CSS,
         );
     }
 }
